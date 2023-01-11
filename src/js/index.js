124a7265d375d3f2c36dd26b7168648e63a51c16
Rafael Ramos <ingrnrg@gmail.com>
2023-01-10 21:14:53 -0600

    added new styles in body & scrollbar

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@
     La letra "o" es convertida para "ober"
     La letra "u" es convertida para "ufat"
  */
+let printText = document.getElementById('console')
 
 const encrypt = () => {
     console.log('encrypt')
@@ -30,11 +31,18 @@ const encrypt = () => {
         }
     }
 
-    // console.log('nuevo arreglo', arreglo)
+    // console.log('nuevo arreglo', arreglo) <p class="result-text">${getText}</p>
     getText = arreglo.join('') // Convetimos el array en un string de nuevo
     console.log("🚀 ~ getText", getText)
-
-    
+    printText.innerHTML=`
+        <div id="result-container">
+            <textarea disable class="result-text" cols="20" rows="22">${getText}</textarea>
+        </div>
+        <div id="copy">
+            <button class="btn btn-copy" onclick="copy()">Copiar</button>
+        </div>
+    `
+    document.getElementById("text").value = ""
 }
 
 const desdesencrypt = () => {
